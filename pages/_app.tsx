1d4d6f780e0c570a93d89751b1a83b4b7c84aab9
muhammadfaruq72 <faruq.72.786@gmail.com>
2022-12-26 19:07:44 +0500

    G analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 import { AuthProvider } from "../components/sections/Global/login_AuthContext";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 
 const httpLink = createUploadLink({
   uri: process.env.NEXT_PUBLIC_BACKEND,
@@ -15,10 +16,13 @@ const client = new ApolloClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ApolloProvider>
+    <>
+      <GoogleAnalytics measurementId="G-3W6Q0V546N" />
+      <ApolloProvider client={client}>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ApolloProvider>
+    </>
   );
 }
